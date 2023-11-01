625c9f42e106663496a4a6c90293d152a60e8f17
aojiaodemeng <845113602@qq.com>
2023-11-01 23:35:51 +0800

    feat: 简易版Card实现，用来搭建文档库

---
+++ src/components/card/index.vue
@@ -0,0 +1,59 @@
+<template>
+  <div :class="['got-card', `${shadow}-shadow`]">
+    <div v-if="$slots.header || header" class="got-card-header">
+      <slot name="header">{{ header }}</slot>
+    </div>
+    <div :style="bodyStyle" class="got-card-body">
+      <slot />
+    </div>
+  </div>
+</template>
+<script lang="ts" setup>
+defineOptions({
+  name: 'GCard'
+})
+
+defineProps({
+  header: {
+    type: String,
+    default: ''
+  },
+  /**
+   * @description CSS style of card body
+   */
+  bodyStyle: {
+    type: String,
+    default: ''
+  },
+  /**
+   * @description custom class name of card body
+   */
+  bodyClass: String,
+  /**
+   * @description when to show card shadows
+   */
+  shadow: {
+    type: String,
+    values: ['always', 'hover', 'never'],
+    default: 'always'
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+.got-card {
+  border-radius: 4px;
+  border: 1px solid rgb(228, 231, 237);
+  overflow: hidden;
+  &.always-shadow {
+    box-shadow: 0px 0px 12px rgba(0, 0, 0, 0.12);
+  }
+
+  .got-card-header {
+    padding: 18px 20px;
+  }
+  .got-card-body {
+    padding: 20px;
+  }
+}
+</style>
---
+++ src/views/card.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import Card from '../components/card/index.vue'
+</script>
+<template>
+  <Card class="box-card">
+    <template #header>
+      <div class="card-header">
+        <span>Card name</span>
+        <el-button class="button" text>Operation button</el-button>
+      </div>
+    </template>
+    <div v-for="o in 4" :key="o" class="text item">{{ 'List item ' + o }}</div>
+  </Card>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
